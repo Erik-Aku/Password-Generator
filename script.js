f82f0f04239b00faa91467f0ebad4a7e7b177486
Erik  Enoka Aku <Erik@Jennifers-iMac.lan>
2023-10-17 09:19:53 -0700

    adding arrays and password length prompt

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var lowerAlpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialChars = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~", "$", "(", ")", ":", ";", "@", "[", "]", "|"]
+
+
+
+function generatePassword() {
+  // prompt user for password length
+  passwordLength = prompt("How long would you like your password to be? Choose between 8 and 128 characters");
+
+  if (!passwordLength) {
+    alert("This is a required field");
+    generatePassword();
+  } else if (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("You must select a value between 8 and 128")
+  }
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
